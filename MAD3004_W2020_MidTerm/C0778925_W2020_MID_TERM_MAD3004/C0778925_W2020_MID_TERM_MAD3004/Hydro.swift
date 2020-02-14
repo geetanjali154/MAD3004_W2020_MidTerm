87540cfb4950c3ba47ac9b06ad45368e8527cc8e
geetanjali <geetanjaligupta154@gmail.com>
2020-02-14 17:56:23 -0500

    hydro init1

---
+++ MAD3004_W2020_MidTerm/C0778925_W2020_MID_TERM_MAD3004/C0778925_W2020_MID_TERM_MAD3004/Hydro.swift
@@ -0,0 +1,38 @@
+//
+//  Hydro.swift
+//  C0778925_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-14.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Hydro:Bill
+{
+var agencyName:String
+var unitComsumed:Int
+var ratePerUnit:Int
+init(billId:Int,billDate:Date,billType:BillType,agencyName:String,unitComsumed:Int,ratePerUnit:Int)
+{
+self.agencyName=agencyName
+self.unitComsumed=unitComsumed
+self.ratePerUnit=ratePerUnit
+super.init(billId:billId,billDate:billDate,billType:billType)
+    }
+
+func getRatePerUnit()->Int
+{
+return self.ratePerUnit
+}
+func setRatePerUnit(ratePerUnit:Int)
+{
+self.ratePerUnit=ratePerUnit
+}
+
+func getUnitComsumed()->Int
+{
+return self.unitComsumed
+}
+func setUnitComsumed(unitComsumed:Int)
+{
+
