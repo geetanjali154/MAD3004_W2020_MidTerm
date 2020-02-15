ad97a8b4ff4210343b817d343446ec46ce912a42
geetanjali <geetanjaligupta154@gmail.com>
2020-02-14 21:16:10 -0500

    changes to main class

---
+++ MAD3004_W2020_MidTerm/C0778925_W2020_MID_TERM_MAD3004/C0778925_W2020_MID_TERM_MAD3004/country.swift
@@ -0,0 +1,19 @@
+//
+//  country.swift
+//  C0778925_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-14.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+class a{
+    func se(){
+var country=["Canada","Abd","India","China","Usa","Nepal"]
+    let s=country.map{(str)->String in
+    if str.count==5
+    {
+        return "\(str.count)-\(str)"
+    }
+    print(s)
+        }
